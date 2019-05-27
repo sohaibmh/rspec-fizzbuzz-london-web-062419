116999b0a8b17bcb694275360befa1503e5da701
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-05-27 11:44:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(x)
+  if x % 15 == 0
+    puts "FizzBuzz"
+  elsif
+    x % 3 == 0
+    puts "Fizz"
+  elsif
+    x % 5 == 0
+    puts "Buzz"
+  end
+end
+
+puts fizzbuzz()
